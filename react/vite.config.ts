24acb85906ef2a08e16d4417fe4d4130568b039f
Anghelo Aguilera <angheloaguilera@gmail.com>
2025-07-21 10:56:54 -0400

    dios

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/a
 export default defineConfig({
+    base: '/react/', // <- esto es MUY importante
   plugins: [react(), tailwindcss()],
 })
